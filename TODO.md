7cb4f7f3e31061a8f24ae2b9808ccc5e1914037d
Tom Duggan <tom13dm@gmail.com>
2024-05-19 01:27:56 -0400

    begin implementing tree chopping logic

--- TODO.md
+++ TODO.md
@@ -16,31 +16,27 @@ your resources
 
 ## TODO ##
 
-### Player ###
-
  - [X] Find some free assets for the player
  - [X] Implement basic movement controls
  - [X] Set up orthogonal camera
  - [X] Add walking animation
  - [X] Switch to click-to-walk / pathfinding
-
-### UI ###
-
- - [ ] Clean up code
- - [ ] Add a click animation
  - [X] Add a custom mouse
-
-### Basic Gameplay ###
-
- - [ ] 3D Tilemaps?
  - [X] Add some tree sprites
- - [ ] Create some collision logic so player doesn't walk through tree
- - [ ] Add some chopping event triggered by clicking the tree
+ - [X] Create some collision logic so player doesn't walk through tree
+ - [X] Register tree click
+ - [X] Make player walk to base of tree when it's clicked on
+ - [ ] Trigger chopping animation when player gets to tree
+
+ - [ ] Give tree proper textures and colors
+ - [ ] Add a click animation
+ - [ ] 3D Tilemaps?
  - [ ] Give the tree a "health bar" and have it decrease when it's being chopped
  - [ ] Switch to a tree stump sprite once the tree's life hits 0
  - [ ] Regenerate the tree after X seconds
  - [ ] Create a basic HUD with a wood icon and quantity
  - [ ] Add wood to your resources when you cut a tree down
+ - [ ] Add mouse wheel camera scroll
 
 ### Bugs ###
 
--- scenes/player.gd
+++ scenes/player.gd
@@ -4,16 +4,18 @@ extends CharacterBody3D
 @export var speed = 4
 @export var fall_acceleration = 75
 @export var camera : Camera3D
-#@export var jump_impulse = 20
-var target_velocity = Vector3.ZERO
 @onready var animation_player = $Pivot/Barbarian/AnimationPlayer
 var walking = false
 var target_position = Vector3.ZERO
-
+var target_velocity = Vector3.ZERO
+var pathfinding_threshold = 0.1
+const TREE_PATHFINDING_THRESHOLD = 1.5
+const GROUND_PATHFINDING_THRESHOLD = 0.1
 
 func _ready():
 	animation_player.play("Walking_B")
 	
+# TODO: Move all this click logic to a separate script
 func _input(event):
 	if event is InputEventMouseButton:
 		if event.pressed:
@@ -26,21 +28,33 @@ func get_world_position(mouse_position):
 	var space = get_world_3d().direct_space_state
 	var ray_query = PhysicsRayQueryParameters3D.new()
 	
-	ray_query.collision_mask = 2
+	ray_query.collision_mask = 0b0011
 	
 	ray_query.from = from
 	ray_query.to = to
 	
 	var result = space.intersect_ray(ray_query)
 	
-	# Set the target position
-	target_position = result.position
-	# Set the flag so that the player starts walking
-	walking = true
-	$Pivot.basis = Basis.looking_at(target_position - position, Vector3.UP, true)
+	if not result.is_empty():
+		var collider : StaticBody3D = result.collider
+		
+		# Clicked on the ground
+		if collider.collision_layer == 1:
+			pathfinding_threshold = GROUND_PATHFINDING_THRESHOLD
+			# Set the target position
+			target_position = result.position
+			# Set the flag so that the player starts walking
+			walking = true
+			$Pivot.basis = Basis.looking_at(target_position - position, Vector3.UP, true)
+		# Clicked on a resource
+		elif collider.collision_layer == 2:
+			pathfinding_threshold = TREE_PATHFINDING_THRESHOLD
+			target_position = collider.position #Vector3(collider.position.x, collider.position.y, collider.position.z)
+			walking = true
+			$Pivot.basis = Basis.looking_at(target_position - position, Vector3.UP, true)
 	
 func reached_destination():
-	return abs(target_position.x - position.x) < 0.1 and abs(target_position.z - position.z) < 0.1
+	return abs(target_position.x - position.x) < pathfinding_threshold and abs(target_position.z - position.z) < pathfinding_threshold
 
 func _physics_process(delta):
 	if walking and not reached_destination():
